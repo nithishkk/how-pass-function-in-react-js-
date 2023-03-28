c0068130c32fa7f3b07181670c3bb93f79506a6a
nithishkk <109412506+nithishkk@users.noreply.github.com>
2023-03-28 10:43:29 +0530

    updateed useCallBack  in react js

--- src/App.js
+++ src/App.js
@@ -5,20 +5,30 @@ import Nithish from './Nithish'
 
 export default function App() {
   const[data,setData]=React.useState("")
-  function Add(a,b){
-    const c = a+b
-    return c
 
-  }
-  Add(1,2)
-   React.useEffect(()=>{
-     setData(Add(1,22))
-   },[])
+
+  const Add = React.useCallback((a,b) => {
+    const c =a+b;
+    return c
+    
+  }, []);
+  React.useEffect(()=>{
+    setData(Add(2,3))
+  },[])
+  // function Add(a,b){
+  //   const c = a+b
+  //   return c
+
+  // }
+  
+  //  React.useEffect(()=>{
+  //    setData(Add(1,22))
+  //  },[])
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
     
-      {Add(1,2)}
+      {Add(1,23)}
       <p>Start editing to see some magic happen :)</p>
       {data}
     </div>
